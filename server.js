ffd77b8929b9453f625b7dc70d6609750516640f
Johan Adamsson <jada@kth.se>
2020-06-10 21:16:06 +0200

    wow, express!

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer } from "apollo-server-express";
 import { typeDefs } from "./typedefs";
 import { resolvers } from "./resolvers";
 import datasources from "./datasources";
 import dotenv from "dotenv";
+const express = require('express');
 
 dotenv.config();
 
@@ -12,6 +13,14 @@ const server = new ApolloServer({
   dataSources: () => ({ ...datasources }),
 });
 
-server.listen().then(({ url }) => {
-  console.log(`🚀 Server ready at ${url}`);
-});
+const app = express();
+
+server.applyMiddleware({ app });
+
+//app.listen().then(({ url }) => {
+//  console.log(`🚀 Server ready at ${url}`);
+//});
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
