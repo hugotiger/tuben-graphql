29595609ce4c65dc474af88606bde7efefb410b0
hugosh <hugosh@kth.se>
2020-06-10 14:56:32 +0200

    Added some code-comments

--- helpers/index.js
+++ helpers/index.js
@@ -1,13 +1,18 @@
-// Separates name and location into separate strings
-// Return value of location defaults to null if it isn't specified
-//
-// TODO: Only separate last parentheses
+/**
+ * Separates name from location written within parentheses into separate strings
+ * Return value of location defaults to null if it isn't specified
+ *
+ * The function assumes that the input has balanced parentheses and no nested parentheses
+ *
+ * @param {string} str
+ */
 export function separateNameAndLocation(str) {
   let name = str;
   let location = null;
 
   let i = str.length - 1;
-  // First check if last character is end parenthesis
+  // First check if last character is end parenthesis,
+  // else we assume location wasn't specified and return null for the location
   if (str[i] === ")") {
     i--;
     for (i; i >= 0; i--) {
