9d2565eee22d1ba0bd8480617c880ffb1e0cfaeb
hugosh <hugosh@kth.se>
2020-06-10 14:50:19 +0200

    Function that separates name and location now works properly if there are multiple parentheses. Also improved time complexity.

--- helpers/index.js
+++ helpers/index.js
@@ -1,8 +1,22 @@
 // Separates name and location into separate strings
 // Return value of location defaults to null if it isn't specified
+//
+// TODO: Only separate last parentheses
 export function separateNameAndLocation(str) {
-  let [name, location] = str.split("(");
-  name = name.trim();
-  location = location ? location.substring(0, location.length - 1) : null;
+  let name = str;
+  let location = null;
+
+  let i = str.length - 1;
+  // First check if last character is end parenthesis
+  if (str[i] === ")") {
+    i--;
+    for (i; i >= 0; i--) {
+      if (str[i] === "(") {
+        name = str.substring(0, i).trim();
+        location = str.substring(i + 1, str.length - 1).trim();
+        break;
+      }
+    }
+  }
   return { name, location };
 }
